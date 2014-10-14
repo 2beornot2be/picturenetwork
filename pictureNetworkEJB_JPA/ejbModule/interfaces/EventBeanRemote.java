3faf6d11b6289e5f51371417417d98fbf05017c1
habbakfarid <farid.habbak@esprit.tn>
2014-10-14 18:49:16 +0100

    modification of Event et creation de ses beans

---
+++ pictureNetworkEJB_JPA/ejbModule/interfaces/EventBeanRemote.java
@@ -0,0 +1,17 @@
+package interfaces;
+
+import javax.ejb.Remote;
+
+import entities.Event;
+
+@Remote
+public interface EventBeanRemote {
+
+	public Event findbyid(int id);
+
+	public void del(int id);
+
+	public void del(Event e);
+
+	public boolean ApproveEvent(Event event, boolean Approve);
+}
